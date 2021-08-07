058a2e38e0bd1b8dbb1dd4bf7be8c27be89975e2
Rathaang <74245440+Rathaang@users.noreply.github.com>
2021-08-07 16:55:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function take_snapshot()
+{
+    save('mushtache.jpg');
+    
+};
+function preload()
+{
+
+};
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+};
+function draw()
+{
+
+};
